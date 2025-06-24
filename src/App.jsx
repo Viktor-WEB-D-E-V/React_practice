82876b3ef2a74a26203a5d8a0705e62a89bd90e0
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-06-23 21:19:44 -0900

    MainGoal component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
+import MainGoal from "./components/MainGoal";
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <MainGoal />
       </main>
     </div>
   );
---
+++ src/components/MainGoal.jsx
@@ -0,0 +1,9 @@
+function MainGoal() {
+  return (
+    <>
+      <p>My main goal: Learn React in-dept and from the ground up</p>
+    </>
+  );
+}
+
+export default MainGoal;
