4ce1a89f5f55e9eb78bf005884f3360ab2d45d19
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-06-24 17:47:19 -0900

    Try adding dynamic data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
+import reactImg from "../assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandom(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
   return (
     <>
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
+          {reactDescriptions[genRandom(2)]} React concepts you will need for
+          almost any app you are going to build!
         </p>
       </header>
     </>
