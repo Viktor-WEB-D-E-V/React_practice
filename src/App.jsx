ba2051a7f9f4fca159b2a17d32200a237110140f
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-06-25 21:26:31 -0900

    Created CoreConcept componet. Code refactoring.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
 //import MainGoal from "./components/MainGoal";
 import { CORE_CONCEPTS } from "./data";
-
-function CoreConcept({ img, title, desc }) {
-  return (
-    <li>
-      <img src={img} alt={title} />
-      <h3>{title}</h3>
-      <p>{desc}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
@@ -20,10 +11,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((concept) => {
+              return <CoreConcept {...concept} />;
+            })}
           </ul>
         </section>
       </main>
